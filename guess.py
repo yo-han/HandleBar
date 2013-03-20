973cb9120b808bcaab864f93febac0f035c9abeb
yo-han <johankuijt@gmail.com>
2013-03-20 09:08:09 +0100

    fix

--- guess.py
+++ guess.py
@@ -3,6 +3,6 @@ from lib import *
 import json, sys
 
 path = sys.argv[1];
-guess = guessit.guess_movie_info(path, info = ['filename'])
+guess = guessit.guess_video_info(path, info = ['filename'])
 
 print json.dumps(guess)
